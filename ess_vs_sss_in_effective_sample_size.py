52529336d144c7adbd1c64a444cdb748d9d144b8
vnataro <vnataro@uni-goettingen.de>
2020-04-20 21:45:09 +0200

    evenly distributed dimensions

--- ess_vs_sss_in_effective_sample_size.py
+++ ess_vs_sss_in_effective_sample_size.py
@@ -11,7 +11,6 @@ from statsmodels.tsa import stattools
 # ray.shutdown()
 # ray.init(object_store_memory=1.5*10**9)
 
-np.random.seed(1000)
 # %% functions
 def pi(x):
     """Calculate value of the density at the point x."""
@@ -24,14 +23,14 @@ def pi2(x, y):
     norm_y = np.linalg.norm(y)
     return np.exp(-norm_x + norm_y + 0.5*(-norm_x**2  + norm_y**2))
 
-def random_MH(size=1, burn_in=0, x0=[0], test_func=None, print_time=True, print_avg_acceptance_rate=True):
+def random_RWM(size=1, burn_in=0, x0=[0], test_func=None, print_time=True, print_avg_acceptance_rate=True):
     """
     Perform the Maetropolis-Hastings Random Walk algorithm w.r.t. the density pi
     "burn_in" + "size" steps starting from x0. Returns a list of sampled vectors
     after burn_in. If "test_func" is given then return test_func(samples).
     """
     start_time = time.time()
-    if print_time: print("MH started.", end=' ')
+    if print_time: print("RWM started.", end=' ')
     d = len(x0)
     if test_func is None: sampled_values = np.zeros((size, len(x0)))
     else:                 sampled_values = np.zeros((size,))
@@ -44,11 +43,10 @@ def random_MH(size=1, burn_in=0, x0=[0], test_func=None, print_time=True, print_
         if i >= 0:
             if test_func is None: sampled_values[i] = x0
             else:                 sampled_values[i] = test_func(x0)
-    if print_time: print ("MH time: %.2f" %(time.time() - start_time))
+    if print_time: print ("RWM time: %.2f" %(time.time() - start_time))
     if print_avg_acceptance_rate:
-        print("MH: average acceptance rate = %.2f" % np.mean(acceptance_rates))
+        print("RWM: average acceptance rate = %.2f" % np.mean(acceptance_rates))
     return sampled_values
-
 
 def runiform_ball(d, R=1, size=1):
     """
@@ -140,25 +138,30 @@ def random_pCN(size=1, burn_in=0, x0=[0], test_func=None, print_time=True, print
     start_time = time.time()
     if print_time: print("pCN started.", end=' ')
     d = len(x0)
-    if test_func is None: sampled_values = np.zeros((size, len(x0)))
+    if test_func is None: sampled_values = np.zeros((size, d))
     else:                 sampled_values = np.zeros((size,))
-    acceptance_rates = np.zeros((burn_in + size,))
+    sum_acceptance_rates = 0
     for i in range(-burn_in, size):
         norm_x0 = np.linalg.norm(x0)
         t = np.random.uniform(0, np.exp(-norm_x0))
+        # t = np.random.uniform(0, np.exp(norm_x0))
         w = np.random.normal(size=d)
 
-        x1 = ellipse_point(x0, w, 10/np.sqrt(d))
-        acceptance_rates[i] = min(1, np.exp(norm_x0 - np.linalg.norm(x1)))
-        x0 = x1 if np.random.uniform() < acceptance_rates[i] else x0
+        # x1 = ellipse_point(x0, w, 10/np.sqrt(d))
+        # x1 = ellipse_point(x0, w, 1)
+        x1 = ellipse_point(x0, w, 1.5 if d <= 100 else 0.3)
+        acceptance_rate = min(1, np.exp(norm_x0 - np.linalg.norm(x1)))
+        # acceptance_rate = min(1, np.exp(-norm_x0 + np.linalg.norm(x1)))
+        x0 = x1 if np.random.uniform() < acceptance_rate else x0
 
         # save values after burn_in
         if i >= 0:
+            sum_acceptance_rates += acceptance_rate
             if test_func is None: sampled_values[i] = x0
             else:                 sampled_values[i] = test_func(x0)
     if print_time: print ("pCN time: %.2f" %(time.time() - start_time))
     if print_avg_acceptance_rate:
-        print("pCN: average acceptance rate = %.2f" % np.mean(acceptance_rates))
+        print("pCN: average acceptance rate = %.2f" % (sum_acceptance_rates / size))
     return sampled_values
 
 def rho(x):
@@ -204,29 +207,32 @@ def effective_sample_size(acf, N):
 
 # %% set initial parameters
 # dimension
-d_range = [40, 100, 400, 1000]
+# d_range = [40, 100, 400, 1000]
+d_range = [10, 30, 100, 300, 1000]
 # d_range = [1000]
 # number of skipped iterations at the beginning
 burn_in = 10**5
 # number of iterations
 N = 10**6
-# set the last k for calculating the autocorrelation function
+# the last k for calculating the autocorrelation function
 k_max = 10**4
 # define the test function for autocorrelation function and effective sample size
-def test_func(samples):
-    """Calculate log(|x|) of each row of the matrix "samples"."""
-    return np.log(1 + np.linalg.norm(samples))
+def test_func(x):
+    """Calculate log(1+|x|)."""
+    return np.log(1 + np.linalg.norm(x))
 
 # %% set full names of the algorithms
 labels = {"SSS" : "Simple slice sampler",
           "iESS": "Idealized elliptical slice sampler",
-          "MH"  : "Metropolis-Hastings",
+          "RWM" : "Metropolis-Hastings",
           "pCN" : "Preconditioned Crank-Nicolson"}
 
 # %% calculating ACF for test_func
+# set random seed
+np.random.seed(1000)
 corr_SSS = np.zeros((len(d_range), k_max))
 corr_ESS = np.zeros((len(d_range), k_max))
-corr_MH  = np.zeros((len(d_range), k_max))
+corr_RWM = np.zeros((len(d_range), k_max))
 corr_pCN = np.zeros((len(d_range), k_max))
 
 for i in range(len(d_range)):
@@ -236,7 +242,7 @@ for i in range(len(d_range)):
     x0 = np.zeros((d,))
     corr_SSS[i] = sample_and_get_acf(random_SSS, N, burn_in, x0, test_func, k_max)
     corr_ESS[i] = sample_and_get_acf(random_ESS, N, burn_in, x0, test_func, k_max)
-    corr_MH [i] = sample_and_get_acf(random_MH,  N, burn_in, x0, test_func, k_max)
+    corr_RWM[i] = sample_and_get_acf(random_RWM, N, burn_in, x0, test_func, k_max)
     corr_pCN[i] = sample_and_get_acf(random_pCN, N, burn_in, x0, test_func, k_max)
 
 # %% plot ACF
@@ -246,7 +252,7 @@ for i in range(len(d_range)):
     d = d_range[i]
     plt.plot(range(1, k_max + 1), corr_SSS[i])
     plt.plot(range(1, k_max + 1), corr_ESS[i])
-    plt.plot(range(1, k_max + 1), corr_MH [i])
+    plt.plot(range(1, k_max + 1), corr_RWM[i])
     plt.plot(range(1, k_max + 1), corr_pCN[i])
     plt.title(f"{d} dimensional ACF for log(1+|x|)")
     plt.xscale("log")
@@ -258,20 +264,20 @@ for i in range(len(d_range)):
 # %% calculating effective sample size
 ess_SSS = list()
 ess_ESS = list()
-ess_MH  = list()
+ess_RWM = list()
 ess_pCN = list()
 
 for i in range(len(d_range)):
     d = d_range[i]
     ess_SSS.append(effective_sample_size(corr_SSS[i], N))
     ess_ESS.append(effective_sample_size(corr_ESS[i], N))
-    ess_MH .append(effective_sample_size(corr_MH [i], N))
+    ess_RWM.append(effective_sample_size(corr_RWM[i], N))
     ess_pCN.append(effective_sample_size(corr_pCN[i], N))
 
 # %% plot effective sample size
 plt.plot(d_range, ess_SSS, '-o')
 plt.plot(d_range, ess_ESS, '-o')
-plt.plot(d_range, ess_MH,  '-o')
+plt.plot(d_range, ess_RWM, '-o')
 plt.plot(d_range, ess_pCN, '-o')
 plt.title("Effective sample size")
 plt.xlabel("Dimension")
@@ -285,17 +291,17 @@ plt.show()
 # print("parallelizing...")
 # p_SSS = random_SSS.remote(N, burn_in, x0)
 # p_ESS = random_ESS.remote(N, burn_in, x0)
-# p_MH  = random_MH .remote(N, burn_in, x0)
+# p_RWM = random_RWM.remote(N, burn_in, x0)
 # p_pCN = random_pCN.remote(N, burn_in, x0)
 #
-# test_SSS, test_ESS, test_MH, test_pCN = ray.get([p_SSS, p_ESS, p_MH, p_pCN])
+# test_SSS, test_ESS, test_RWM, test_pCN = ray.get([p_SSS, p_ESS, p_RWM, p_pCN])
 # print("Done.")
 # ray.shutdown()
 
 # %% test algorithms
 # test_SSS = random_SSS(N, burn_in, x0)
 # test_ESS = random_ESS(N, burn_in, x0)
-# test_MH  = random_MH (N, burn_in, x0)
+# test_RWM = random_RWM(N, burn_in, x0)
 # test_pCN = random_pCN(N, burn_in, x0)
 
 # %% save the kernel state
@@ -306,22 +312,22 @@ plt.show()
 # dill.load_session("sss_vs_ess_kernel.db")
 
 # %% drawing one histogram of the first coordinates with the target density
-# values = [test_SSS[:, 0], test_ESS[:, 0], test_MH[:, 0], test_pCN[:, 0]]
+# values = [test_SSS[:, 0], test_ESS[:, 0], test_RWM[:, 0], test_pCN[:, 0]]
 # count, bins, ignored = plt.hist(values, bins=20, density=True)
 # plt.legend(labels.keys())
 # plt.show()
 
 # %% drawing separate histograms of the first coordinates with the target density
 # draw_histogram_check(test_SSS[:, 0], labels["SSS"])
-# draw_histogram_check(test_ESS[:, 0], labels["ESS"])
-# draw_histogram_check(test_MH [:, 0], labels["MH"])
+# draw_histogram_check(test_ESS[:, 0], labels["iESS"])
+# draw_histogram_check(test_RWM[:, 0], labels["RWM"])
 # draw_histogram_check(test_pCN[:, 0], labels["pCN"])
 
 # %% simulating and drawing one path of the first coordinate of each algorithm
 # sample_and_draw_path(random_SSS, labels["SSS"], x0, 1000)
 # sample_and_draw_path(random_ESS, labels["ESS"], x0, 1000)
-# sample_and_draw_path(random_MH,  labels["MH"],  x0, 1000)
-# x0 = np.zeros((1000))
+# sample_and_draw_path(random_RWM, labels["RWM"], x0, 1000)
+# x0 = np.zeros((40))
 # x0[0]=0
 # sample_and_draw_path(random_pCN, labels["pCN"], x0, 100000)
 
