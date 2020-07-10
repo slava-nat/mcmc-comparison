13737ef5d3f2c2145d41b899f5b16ad6582e50d4
vnataro <vnataro@uni-goettingen.de>
2020-07-10 16:11:55 +0200

    generalized algs + results for gaussian mixture

--- effective_sample_size.py
+++ effective_sample_size.py
@@ -15,47 +15,78 @@ import mcmc
 # %% set initial parameters
 # dimension
 d_range = [10, 30, 100, 300, 1000]
+# d_range = [10, 30]
 # number of skipped iterations at the beginning
-burn_in = 10**5
+burn_in = 10**4
 # number of iterations
-N = 10**6
+N = 10**5
 # the last k for calculating the autocorrelation function
 k_max = 10**4
 
-# define the density function
+# define Gaussian mixture with two peaks centered in
+# m1 = (0, ..., 0) and m2 = (1, ..., 1)
+# define standard deviations
+sd1 = 1
+sd2 = 1
+# define weights. must sum to 1
+weight1 = 0.5
+weight2 = 1 - weight1
+
+# define multivariate gaussian PDF
 @njit
-def density_func(x):
-    """Calculate value of the density at the point x."""
-    norm_x = np.linalg.norm(x)
-    return np.exp(norm_x - 0.5 * norm_x**2)
+def multivariate_normal_pdf(x, mean, sd):
+    d = len(x)
+    norm_constant = sd**d
+    return np.exp(-0.5 * np.linalg.norm(x - mean)**2 / sd**2) / norm_constant
+
+# define log of the density function
+@njit
+def ln_pdf(x):
+    d = len(x)
+    # define the means
+    m1 = np.zeros(d)
+    m1[0] = d
+    m2 = -m1
+    # define two gaussians
+    g1 = multivariate_normal_pdf(x, m1, sd1)
+    g2 = multivariate_normal_pdf(x, m2, sd2)
+
+    return np.log(weight1 * g1 + weight2 * g2)
+
+# define log of the volcano density function
+# @njit
+# def ln_pdf(x):
+    # """Calculate log value of the density at the point x."""
+    # norm_x = np.linalg.norm(x)
+    # return norm_x - 0.5 * norm_x**2
 
 # define the function for uniform sampling on the level set.
 # Needed in simple slice sampler
-@njit
-def runiform_levelset(d, t):
-    """
-    Sample uniformly on a level-set according to the level "t"
-    with dimension "d" for "density_func".
-    """
-    # define neccessary function
-    @njit
-    def runiform_disc(d, R=1, r=0):
-        """
-        Sample efficiently from a uniform distribution on a
-        d-dimensional disc centered in zero
-        D(R, r) = {x : r < |x| < R}.
-        """
-        x = np.random.normal(0, 1, size=d)
-        # if r == 0 then sample more efficiently on a ball
-        if r == 0:
-            u = np.random.uniform(0, 1)
-            return R * u**(1/d) * x / np.linalg.norm(x)
-        # otherwise sample on a disc
-        u = np.random.uniform(r**d, R**d)
-        return u**(1/d) * x / np.linalg.norm(x)
+# @njit
+# def runiform_levelset(d, t):
+#     """
+#     Sample uniformly on a level-set according to the level "t"
+#     with dimension "d" for "density_func".
+#     """
+#     # define neccessary function
+#     @njit
+#     def runiform_disc(d, R=1, r=0):
+#         """
+#         Sample efficiently from a uniform distribution on a
+#         d-dimensional disc centered in zero
+#         D(R, r) = {x : r < |x| < R}.
+#         """
+#         x = np.random.normal(0, 1, size=d)
+#         # if r == 0 then sample more efficiently on a ball
+#         if r == 0:
+#             u = np.random.uniform(0, 1)
+#             return R * u**(1/d) * x / np.linalg.norm(x)
+#         # otherwise sample on a disc
+#         u = np.random.uniform(r**d, R**d)
+#         return u**(1/d) * x / np.linalg.norm(x)
     
-    R = 1 + np.sqrt(1 - 2*np.log(t))
-    return runiform_disc(d, R, max(0, 2 - R))
+#     R = 1 + np.sqrt(1 - 2*np.log(t))
+#     return runiform_disc(d, R, max(0, 2 - R))
 
 # define the test function for autocorrelation function and effective sample size
 @njit
@@ -76,12 +107,24 @@ acfs = {}
 for d in d_range:
     print("Start computing for d =", d)
     # starting vector of dimension "d"
-    x0 = np.zeros((d,))
+    x0 = np.zeros(d)
+    x0[0] = d
     acfs[d] = {}
     for key in algorithms.keys():
+        args = {"ln_pdf": ln_pdf,
+                "size": N,
+                "burn_in": burn_in,
+                "x0": x0,
+                "test_func": test_func}
+
+        if key == "RWM":
+            args["cov_matrix"] = 2.4 / np.sqrt(d) * np.identity(d)
+        if key == "pCN":
+            args["angle_par"] = np.pi + 2.4 / d
+
         start_time = time.time()
         print(f"{key} started.")
-        samples = algorithms[key](density_func, N, burn_in, x0, test_func=test_func)
+        samples = algorithms[key](**args)
         print(f"{key} time: {time.time() - start_time}")
         acfs[d][key] = stattools.acf(samples, nlags=k_max, fft=True)[1:]
 
@@ -93,8 +136,8 @@ for alg in algorithms.keys():
     ess[alg] = [N / (1 + 2 * np.sum(acfs[d][alg])) for d in d_range]
 
 # %% save the kernel state
-import dill
-dill.dump_session("sss_vs_ess_kernel.db")
+# import dill
+# dill.dump_session("sss_vs_ess_kernel.db")
 
 # %% load the kernel state if needed
 # import dill
@@ -113,7 +156,6 @@ for d in d_range:
 
 # %% plot effective sample size
 for alg in algorithms.keys():
-# for alg in ["SSS", "iESS"]:
     plt.plot(d_range, ess[alg], '-o')
 plt.title("Effective sample size")
 plt.xlabel("Dimension")
@@ -135,45 +177,32 @@ plt.show()
 # plt.savefig("pics/ESS_corrected.pdf")
 # plt.show()
 
-# %% test algorithms
-# x0 = np.zeros((2,))
-# test_SSS  = random_SSS (N, burn_in, x0)
-# test_iESS = random_iESS(N, burn_in, x0)
-# test_ESS  = random_ESS (N, burn_in, x0)
-# test_RWM  = random_RWM (N, burn_in, x0)
-# test_pCN  = random_pCN (N, burn_in, x0)
-
-
-# %% drawing one histogram of the first coordinates with the target density
-# values = [test_SSS, test_iESS, test_ESS, test_RWM, test_pCN]
-# count, bins, ignored = plt.hist(values, bins=20, density=True)
-# plt.legend(algorithms.keys())
-# plt.show()
-
-# %% drawing separate histograms of the first coordinates with the target density
-# draw_histogram_check(test_SSS,  "SSS")
-# draw_histogram_check(test_iESS, "iESS")
-# draw_histogram_check(test_ESS,  "ESS")
-# draw_histogram_check(test_RWM,  "RWM")
-# draw_histogram_check(test_pCN,  "pCN")
-# h1 = plt.hist2d(test_SSS [:, 0], test_SSS [:, 1], bins=50)
-# h2 = plt.hist2d(test_iESS[:, 0], test_iESS[:, 1], bins=50)
-# h3 = plt.hist2d(test_RWM [:, 0], test_RWM [:, 1], bins=50)
-# h4 = plt.hist2d(test_pCN [:, 0], test_pCN [:, 1], bins=50)
-
-
 # %% simulating and drawing one path of the first coordinate of each algorithm
-# sample_and_draw_path(random_SSS,  labels["SSS"],  x0, 1000)
-# sample_and_draw_path(random_iESS, labels["iESS"], x0, 1000)
-# sample_and_draw_path(random_ESS, "ESS", np.zeros((10)), 1000)
-# sample_and_draw_path(random_RWM2,  "RWM",  np.zeros(10), 1000)
-# sample_and_draw_path(random_pCN,  labels["pCN"],  x0, 1000)
+# d = 100
+# x0 = np.zeros(d)
+# x0[0] = d
+# steps = 10**5
 
-# %% tune acceptance probability of RWM
+# args = {'ln_pdf': ln_pdf,
+#         'size': steps,
+#         'x0': x0}
+
+# args_RWM = {**args, **{'cov_matrix': 2.4 / np.sqrt(d) * np.identity(d)}}
+# args_pCN = {**args, **{'angle_par': np.pi + 2.4 / d}}
+
+# print(f"start for d = {d}. x0 is the positive peak. {steps} steps")
+# mcmc.sample_and_draw_path(mcmc.random_ESS, "ESS", **args)
+# mcmc.sample_and_draw_path(mcmc.random_RWM, "RWM", **args_RWM)
+# mcmc.sample_and_draw_path(mcmc.random_pCN, "pCN", **args_pCN)
+# %% tune acceptance probability
+# d = 100
+# x0 = np.zeros(d)
+# x0[0] = d
 # burn_in = 10**4
 # N = 10**5
-# d = 10
-# x0 = np.zeros(d)
-# for par in np.arange(0.79, 0.91, 0.05):
+
+# for par in np.arange(100, 200, 50):
 #     print(f"par = {par}")
-#     random_RWM(par, N, burn_in, x0, test_func, print_time=False)
+#     mcmc.sample_and_draw_path(mcmc.random_RWM, "RWM",
+#                               ln_pdf=ln_pdf, size=N, burn_in=burn_in, x0=x0,
+#                               cov_matrix=par * np.identity(d))
